de7521f08de0b46da2ae74e0990260de1dd0ac90
Jonathas <Jonathas@iNTeR-sEct.Home>
2018-06-11 11:15:42 -0300

    closed #12

--- barzinhoGo/src/main/java/org/barzinhogo/model/Cliente.java
+++ barzinhoGo/src/main/java/org/barzinhogo/model/Cliente.java
@@ -1,108 +1,96 @@
 package org.barzinhogo.model;
 
-import javax.persistence.Entity;
 import java.io.Serializable;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.Version;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 
+ * @author Adriano
+ */
 @Entity
+@Table(name = "cliente")
 public class Cliente implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
-	private Long id;
-	@Version
-	@Column(name = "version")
-	private int version;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	@Column(nullable = false)
-	private String nome;
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
 
-	@Column(nullable = false)
-	private String email;
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
 
-	@Column(nullable = false)
-	private String senha;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Long getId() {
-		return this.id;
-	}
+    @Column(length = 100, nullable = false)
+    public String getNome() {
+        return nome;
+    }
 
-	public void setId(final Long id) {
-		this.id = id;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public int getVersion() {
-		return this.version;
-	}
+    @Column(length = 30, nullable = false)
+    public String getEmail() {
+        return email;
+    }
 
-	public void setVersion(final int version) {
-		this.version = version;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Cliente)) {
-			return false;
-		}
-		Cliente other = (Cliente) obj;
-		if (id != null) {
-			if (!id.equals(other.id)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    @Column(length = 10, nullable = false)
+    public String getSenha() {
+        return senha;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override
-	public String toString() {
-		String result = getClass().getSimpleName() + " ";
-		if (nome != null && !nome.trim().isEmpty())
-			result += "nome: " + nome;
-		if (email != null && !email.trim().isEmpty())
-			result += ", email: " + email;
-		if (senha != null && !senha.trim().isEmpty())
-			result += ", senha: " + senha;
-		return result;
-	}
 }
