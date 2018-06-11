c54c98604a4d638c1ab17eb9de4e8b37e6c7e3f0
Jonathas <Jonathas@192.168.1.11>
2018-06-11 15:56:32 -0300

    Closed #1

---
+++ barzinhoGo/src/main/java/org/barzinho/controller/CriarTabelas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.barzinho.controller;
+
+import javax.persistence.Persistence;
+
+/**
+ * Classe para a criação e atualização das tabelas no banco de dados por meio da
+ * EntityManangerFactory 
+ *
+ */
+public class CriarTabelas {
+
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("comandasGoCore");
+    }
+}
